01b7462ac33e0e9f95cbb904a52d6e278403eb35
Lars Op den Kamp <lars@opdenkamp.eu>
2025-03-10 19:19:18 +0100

    added: ir frame info

--- mx_remote/proto/FrameRCIr.py
+++ mx_remote/proto/FrameRCIr.py
@@ -7,11 +7,33 @@
 
 from .FrameBase import FrameBase
 from .FrameHeader import FrameHeader
+from ..Interface import BayBase
+import struct
+import logging
+
+_LOGGER = logging.getLogger(__name__)
 
 class FrameRCIr(FrameBase):
     ''' IR key press '''
     def __init__(self, header:FrameHeader):
         super().__init__(header)
+
+    @property
+    def bay(self) -> BayBase:
+        # bay that received the key press
+        dev = self.remote_device
+        if dev is None:
+            return None
+        portnum = ((int(self.payload[1]) << 8) | int(self.payload[0]))
+        return dev.get_by_portnum(portnum)
+
+    @property
+    def timestamp(self) -> int:
+        return struct.unpack('<L', self.payload[2:6])[0]
+
+    def process(self) -> None:
+        bay = self.bay
+        _LOGGER.debug(f"IR key bay {self.bay} timestamp {self.timestamp}")
 
     def __str__(self) -> str:
         return f"IR key press"
